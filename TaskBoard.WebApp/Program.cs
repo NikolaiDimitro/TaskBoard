203044f28326b1d54d8919e038cd1a4f9899a2b9
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-03-23 19:07:21 +0200

    Update

--- TaskBoard.WebApp/Program.cs
+++ TaskBoard.WebApp/Program.cs
@@ -1,5 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Text.RegularExpressions;
 
+using TaskBoard.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -8,9 +14,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 if (!string.IsNullOrEmpty(connectionString))
 {
     // Remove MultipleActiveResultSets parameter (case-insensitive)
-    connectionString  = Regex.Replace(connectionString, @"(?i)MultipleActiveResultSets\s*=\s*(?:true|false|yes|no)\s*;?", "", RegexOptions.IgnoreCase);
+    connectionString = Regex.Replace(connectionString, @"(?i)MultipleActiveResultSets\s*=\s*(?:true|false|yes|no)\s*;?", "", RegexOptions.IgnoreCase);
     // Clean up any double semicolons or trailing semicolons
-    connectionString = connectionString.Replace(";;", ";").TrimEnd(';');
+    connectionString = connectionString.Replace(";;", ";").TrimEnd(57);
 }
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
